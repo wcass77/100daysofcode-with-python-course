32d3b0e258196c270a773bb2c96e6358b1665792
wcass77 <wcass77@users.noreply.github.com>
2018-10-29 15:39:26 -0400

    Identified areas to handle as errors tomorrow

--- days/25-27-error-handling/dnd_game/program.py
+++ days/25-27-error-handling/dnd_game/program.py
@@ -1,5 +1,14 @@
-from actors import Creature, Wizard, Dragon
+"""
+List of potential error states:
+The user enters something invalid: The program should recover, list options
+The program should also respond correctly if you type out command
+Keyboard interrupt: Program should exit gracefully
+Life is less than 0: Should tell you that you're dead and exit
+"""
 import random
+
+from actors import Creature, Dragon, Wizard
+
 
 def main():
     print_header()
@@ -7,47 +16,51 @@ def main():
 
 
 def print_header():
-    print('---------------------------------')
-    print('          WIZARD GAME')
-    print('---------------------------------')
+    print("---------------------------------")
+    print("          WIZARD GAME")
+    print("---------------------------------")
     print()
 
 
 def game_loop():
     creatures = [
-        Creature('Bat', 5),
-        Creature('Toad', 1),
-        Creature('Tiger', 12),
-        Dragon('Black Dragon', 50, scaliness=2, breaths_fire=False),
-        Wizard('Evil wizard', 1000),
+        Creature("Bat", 5),
+        Creature("Toad", 1),
+        Creature("Tiger", 12),
+        Dragon("Black Dragon", 50, scaliness=2, breaths_fire=False),
+        Wizard("Evil wizard", 1000),
     ]
 
-    hero = Wizard('Gandolf', 75)
+    hero = Wizard("Gandolf", 75)
 
     while True:
 
         active_creature = random.choice(creatures)
 
-        print('A {} of level {} has appear from a dark and foggy forest...'
-              .format(active_creature.name, active_creature.level))
+        print(
+            "A {} of level {} has appear from a dark and foggy forest...".format(
+                active_creature.name, active_creature.level
+            )
+        )
         print()
 
-        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
-        if cmd == 'a':
+        cmd = input("Do you [a]ttack, [r]unaway, or [l]ook around? ")
+        if cmd == "a":
             if hero.attack(active_creature):
                 creatures.remove(active_creature)
                 print("The wizard defeated {}".format(active_creature.name))
             else:
-                print("The wizard has been defeat by the powerful {}".format(active_creature.name))
-        elif cmd == 'r':
-            print('The wizard has become unsure of his power and flees!!!')
-        elif cmd == 'l':
-            print('The wizard {} takes in the surroundings and sees:'
-                  .format(hero.name))
+                print(
+                    "The wizard has been defeat by the powerful {}".format(
+                        active_creature.name
+                    )
+                )
+        elif cmd == "r":
+            print("The wizard has become unsure of his power and flees!!!")
+        elif cmd == "l":
+            print("The wizard {} takes in the surroundings and sees:".format(hero.name))
             for c in creatures:
-                print(" * {} of level {}".format(
-                    c.name, c.level
-                ))
+                print(" * {} of level {}".format(c.name, c.level))
         else:
             print("OK, exiting game... bye!")
             break
@@ -59,5 +72,5 @@ def game_loop():
         print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
