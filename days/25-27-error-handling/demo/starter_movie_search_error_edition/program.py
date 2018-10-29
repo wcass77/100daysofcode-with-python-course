ace954508b1706651afa3f0b7c6bcdbd803a7504
wcass77 <wcass77@users.noreply.github.com>
2018-10-29 15:33:27 -0400

    Day 25 exploration

--- days/25-27-error-handling/demo/starter_movie_search_error_edition/program.py
+++ days/25-27-error-handling/demo/starter_movie_search_error_edition/program.py
@@ -2,13 +2,13 @@ import api
 
 
 def main():
-    keyword = 'Python' # input('Keyword of title search: ')
+    keyword = input("Keyword of title search: ")
     results = api.find_movie_by_title(keyword)
 
-    print(f'There are {len(results)} movies found.')
+    print(f"There are {len(results)} movies found.")
     for r in results:
         print(f"{r.title} with code {r.imdb_code} has score {r.imdb_score}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
