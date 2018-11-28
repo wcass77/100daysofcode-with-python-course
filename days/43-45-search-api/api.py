e488d02ae4fc0a211132661e2eb418dca3a77d66
wcass77 <wcass77@users.noreply.github.com>
2018-11-27 21:00:17 -0500

    changed json to use request.json()

--- days/43-45-search-api/api.py
+++ days/43-45-search-api/api.py
@@ -1,4 +1,3 @@
-import json
 from typing import List
 
 import requests
@@ -8,7 +7,7 @@ URL = "http://search.talkpython.fm/api/search?q="
 
 def search(search_terms: List[str], item_type: str = "Episode") -> List[dict]:
     param = "-".join(search_terms)
-    results = json.loads(requests.get(URL + param).text)
+    results = requests.get(URL + param).json()
     results = [
         result for result in results["results"] if result["category"] == item_type
     ]
